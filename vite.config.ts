bf04b06a24f34a0ac996724df7a52f6a47b37417
ouafid <mohamedhabchi@teccart.online>
2025-02-20 13:20:24 -0500

    routing dans le vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+// Configuration de Vite pour un projet avec des ressources statiques
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    // Définit le répertoire de sortie pour le build
+    outDir: 'dist', // Ce répertoire contiendra tous vos fichiers statiques générés
+    assetsDir: 'assets', // Spécifie où les ressources comme les images et les fichiers CSS/JS seront placées
+    rollupOptions: {
+      input: './index.html', // Vous pouvez spécifier un fichier d'entrée si nécessaire
+    },
+    // Options supplémentaires pour améliorer l'expérience de build
+    minify: 'esbuild', // Optimisation pour minimiser les fichiers
+    sourcemap: true, // Générer des sourcemaps si nécessaire (utile pour le débogage)
+  }
 })
